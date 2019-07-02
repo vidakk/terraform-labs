46607d958701cd685e303fef7391e2eafc3e09bb
Vidak Kavedzic <v.kavedzic@levi9.com>
2019-07-02 15:13:24 +0200

    initial commit

---
+++ provider.tf
@@ -0,0 +1,6 @@
+provider "azurerm" {
+  subscription_id = "ca0dcd82-d218-4396-8e41-1d81be1eb240"
+  client_id       = "35dba25f-d375-4b48-a18e-e2d4f2c81586"
+  client_secret   = "152fa89a-fae1-4e26-9546-445fabca00f9"
+  tenant_id       = "40758481-7365-442c-ae94-563ed1606218"
+}
